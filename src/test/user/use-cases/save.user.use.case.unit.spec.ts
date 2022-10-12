2adcf2306bbd0b95cec4f0432c6a10b38287b5ac
Emmanuel Castillo Vega <emmanuelcastillovega5@gmail.com>
2022-10-12 16:20:20 -0500

    test: a use case from the save user unit test was fixed

--- src/test/user/use-cases/save.user.use.case.unit.spec.ts
+++ src/test/user/use-cases/save.user.use.case.unit.spec.ts
@@ -1,13 +1,9 @@
 import { SaveUserUseCase } from "../../../user/application/use-cases/save-user.use.case";
-import {
-  UserEntity,
-  UserPrimitive,
-} from "../../../user/domain/user/user.aggregate";
+import { UserEntity } from "../../../user/domain/user/user.aggregate";
 import { UserMockRepository } from "../../../user/infrastructure/repositories/user.mock.repository";
 
 describe("SaveUser-Use-Case", () => {
   let useCase: SaveUserUseCase;
-  let newUser: UserEntity;
 
   beforeEach(() => {});
 
@@ -20,6 +16,42 @@ describe("SaveUser-Use-Case", () => {
   });
 
   it("Should save user in the database", async () => {
-    const response = await useCase.execute(newUser);
+    const response = await useCase.execute(
+      UserEntity.fromPrimitive(
+        "123,456",
+        "Emmanuel",
+        2,
+        8,
+        "Emma123",
+        "EmmaCV15",
+        "1665109273499",
+        "1665109273499"
+      )
+    );
+
+    const {
+      contain,
+      status,
+      success,
+    }: {
+      contain: any;
+      status: number;
+      success: boolean;
+    } = response.toPrimitives();
+    expect(contain).toEqual({
+      user: {
+        id: "123-456-789",
+        name: "Test",
+        userId: 1,
+        achievements: 10,
+        username: "root",
+        password: "123",
+        createdAt: "1665109273499",
+        updatedAt: "1665109273499",
+      },
+    });
+
+    expect(status).toEqual(200);
+    expect(success).toEqual(true);
   });
 });
